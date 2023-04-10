6b5806eaf4504fe9e7e527c3e5773bd2fa7899ba
stevengborrego <sborrego@pdx.edu>
2023-04-10 13:36:12 -0700

    implement palindrome, styling

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,27 @@
 const elem = document.querySelector('input');
 
 elem.addEventListener('input', handleInput);
+
+function handleInput(event) {
+    const inputNum = Number(elem.value);
+
+    if (inputNum > 0) {
+        // reverse the input
+        reverseInput = elem.value.split("");
+        reverseInput.reverse();
+        reverseInput = reverseInput.join("");
+        console.log(reverseInput);
+        // check if palindrome
+        if (elem.value === reverseInput) {
+            console.log(true);
+            document.getElementById("result").className = "text-success";
+            document.getElementById("result").innerHTML = "Yes. This is a palindrome!";
+        }
+        else {
+            console.log(false);
+            document.getElementById("result").className = "text-danger";
+            document.getElementById("result").innerHTML = "No, Try again.";
+        }
+    }
+    
+}
