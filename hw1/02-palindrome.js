e3516b7d090e9646515aadf5039bf6d7b174a201
stevengborrego <sborrego@pdx.edu>
2023-04-12 12:05:46 -0700

    update formatting

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,25 +1,27 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
 function handleInput() {
   const inputNum = Number(elem.value);
 
   if (inputNum > 0) {
     // reverse the input
-    let reverseInput = elem.value.split('');
+    let reverseInput = elem.value.split("");
     reverseInput.reverse();
-    reverseInput = reverseInput.join('');
+    reverseInput = reverseInput.join("");
     // check if palindrome
     if (elem.value === reverseInput) {
-      document.getElementById('result').className = 'text-success';
-      document.getElementById('result').innerHTML = 'Yes. This is a palindrome!';
+      document.getElementById("result").className = "text-success";
+      document.getElementById("result").innerHTML =
+        "Yes. This is a palindrome!";
     } else {
-      document.getElementById('result').className = 'text-danger';
-      document.getElementById('result').innerHTML = 'No, Try again.';
+      document.getElementById("result").className = "text-danger";
+      document.getElementById("result").innerHTML = "No, Try again.";
     }
   } else {
-    document.getElementById('result').className = 'text-danger';
-    document.getElementById('result').innerHTML = 'Please enter a positive number.';
+    document.getElementById("result").className = "text-danger";
+    document.getElementById("result").innerHTML =
+      "Please enter a positive number.";
   }
 }
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
