8373ab05edb3fe4c6272a9ffd8785f24de200d6c
stevengborrego <sborrego@pdx.edu>
2023-04-25 15:17:13 -0700

    fix keyup/keydown

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,17 @@
-// Add your code here
+const input = document.querySelector("input");
 
+
+function handleKeyDown() {
+  const inputStr = input.value;
+  //console.log(inputStr);
+}
+
+function handleKeyUp() {
+  const inputStr = input.value.trim();
+  console.log(inputStr);
+
+  //https://dev.to/kokaneka/highlight-searched-text-on-a-page-with-just-javascript-17b3#:~:text=The%20'mark'%20tag&text=If%20you%20surround%20any%20text,in%20this%20striking%20yellow%20color.&text=That%20makes%20highlighting%20searched%20text%20quite%20a%20simple%20task%20then.
+}
+
+input.addEventListener('keyup', handleKeyUp);
 input.addEventListener('keydown', handleKeyDown);
