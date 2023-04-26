952037ac2a23ffc9d3dd31c845bc63d685a1de15
stevengborrego <sborrego@pdx.edu>
2023-04-25 17:44:40 -0700

    implement 03-count

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,17 +1,14 @@
 const input = document.querySelector("input");
-
-
-function handleKeyDown() {
-  const inputStr = input.value;
-  //console.log(inputStr);
-}
+var originalText = document.getElementById('text').innerHTML; 
 
 function handleKeyUp() {
-  const inputStr = input.value.trim();
-  console.log(inputStr);
-
-  //https://dev.to/kokaneka/highlight-searched-text-on-a-page-with-just-javascript-17b3#:~:text=The%20'mark'%20tag&text=If%20you%20surround%20any%20text,in%20this%20striking%20yellow%20color.&text=That%20makes%20highlighting%20searched%20text%20quite%20a%20simple%20task%20then.
+  let inputStr = input.value.trim();  
+  if (inputStr !== '') {
+    let text = originalText;
+    let regex = new RegExp(inputStr,"g");
+    let newText = text.replace(regex, `<mark>${inputStr}</mark>`);
+    document.getElementById('text').innerHTML = newText;
+  }
 }
 
 input.addEventListener('keyup', handleKeyUp);
-input.addEventListener('keydown', handleKeyDown);
