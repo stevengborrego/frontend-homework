97f3d838745267913a9a3617b7069f48e813fcaa
stevengborrego <sborrego@pdx.edu>
2023-05-03 16:33:06 -0700

    implement 02-charts

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,42 +1,33 @@
-const character = document.getElementsByClassName('character');
+const character = document.getElementsByClassName("character");
 
 async function getCharacters() {
-  const url = 'https://thronesapi.com/api/v2/Characters';
+  const url = "https://thronesapi.com/api/v2/Characters";
   try {
     let response = await fetch(url);
     return await response.json();
-  }
-  catch (erorr) {
+  } catch (erorr) {
     console.log(error);
   }
 }
 
 async function renderCharacters() {
   let characters = await getCharacters();
-  let html = '';
+  let html = "";
 
-  characters.forEach(character => {
-    let htmlSegment = `<div class="character" onmouseover="this.style.background='gray';" onmouseout="this.style.background='none';">
+  characters.forEach((character) => {
+    let htmlSegment = `<div class="character" onmouseover="this.style.background='var(--blue)';" onmouseout="this.style.background='none';">
                         <div style="margin:1em;">
                           <img src="${character.imageUrl}" >
                           <h3>${character.firstName} ${character.lastName}</h3>
                           <h4>${character.title}</h4>
                         </div>
                       </div>`;
-                      
+
     html += htmlSegment;
   });
 
-  let section = document.querySelector('section');
+  let section = document.querySelector("section");
   section.innerHTML = html;
 }
 
-function changeBackground() {
-
-}
-
 renderCharacters();
-
-character.addEventListener("mouseover", changeBackground)
-
-
--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -1,45 +1,96 @@
 const backgroundColors = [
-  'rgba(54, 162, 235, 0.8)',
-  'rgba(255, 206, 86, 0.8)',
-  'rgba(255, 99, 132, 0.8)',
-  'rgba(75, 192, 192, 0.8)',
-  'rgba(153, 102, 255, 0.8)',
-  'rgba(255, 159, 64, 0.8)',
-  'rgba(199, 199, 199, 0.8)',
-  'rgba(83, 102, 255, 0.8)',
-  'rgba(40, 159, 64, 0.8)',
-  'rgba(210, 199, 199, 0.8)',
-  'rgba(78, 52, 199, 0.8)',
+  "rgba(54, 162, 235, 0.8)",
+  "rgba(255, 206, 86, 0.8)",
+  "rgba(255, 99, 132, 0.8)",
+  "rgba(75, 192, 192, 0.8)",
+  "rgba(153, 102, 255, 0.8)",
+  "rgba(255, 159, 64, 0.8)",
+  "rgba(199, 199, 199, 0.8)",
+  "rgba(83, 102, 255, 0.8)",
+  "rgba(40, 159, 64, 0.8)",
+  "rgba(210, 199, 199, 0.8)",
+  "rgba(78, 52, 199, 0.8)",
 ];
 
 const borderColors = [
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)',
-  'rgba(255, 99, 132, 1)',
-  'rgba(75, 192, 192, 1)',
-  'rgba(153, 102, 255, 1)',
-  'rgba(255, 159, 64, 1)',
-  'rgba(159, 159, 159, 1)',
-  'rgba(83, 102, 255, 1)',
-  'rgba(40, 159, 64, 1)',
-  'rgba(210, 199, 199, 1)',
-  'rgba(78, 52, 199, 1)',
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+  "rgba(255, 99, 132, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(255, 159, 64, 1)",
+  "rgba(159, 159, 159, 1)",
+  "rgba(83, 102, 255, 1)",
+  "rgba(40, 159, 64, 1)",
+  "rgba(210, 199, 199, 1)",
+  "rgba(78, 52, 199, 1)",
 ];
 
 // url for the Thrones API
-const url = 'https://thronesapi.com/api/v2/Characters';
+const url = "https://thronesapi.com/api/v2/Characters";
 
-const renderChart = () => {
-  const donutChart = document.querySelector('.donut-chart');
+async function renderChart() {
+  console.log("in render chart");
+  const donutChart = document.querySelector(".donut-chart");
+
+  let houseCount = {
+    Stark: 0,
+    Arryn: 0,
+    Baratheon: 0,
+    Tully: 0,
+    Greyjoy: 0,
+    Lannister: 0,
+    Tyrell: 0,
+    Martell: 0,
+    Targaryen: 0,
+  };
+  const characters = await  getCharacters();
+  console.log(characters);
+
+  characters.forEach((character) => {
+    if (character.family.includes("Stark")) houseCount.Stark += 1;
+    else if (character.family.includes("Arryn")) houseCount.Arryn += 1;
+    else if (character.family.includes("Baratheon"))  houseCount.Baratheon += 1;
+    else if (character.family.includes("Tully")) houseCount.Tully += 1;
+    else if (character.family.includes("Greyjoy")) houseCount.Greyjoy += 1;
+    else if (character.family.includes("Lannister")) houseCount.Lannister += 1;
+    else if (character.family.includes("Tyrell")) houseCount.Tyrell += 1;
+    else if (character.family.includes("Martell")) houseCount.Martell += 1;
+    else if (
+      character.family.includes("Targaryen") ||
+      character.family.includes("Targaryn")
+    )
+      houseCount.Targaryen += 1;
+  });
 
   new Chart(donutChart, {
-    type: 'doughnut',
+    type: "doughnut",
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: [
+        "House Stark",
+        "House Arryn",
+        "House Baratheon",
+        "House Tully",
+        "House Greyjoy",
+        "House Lannister",
+        "House Tyrell",
+        "House Martell",
+        "House Targaryen",
+      ],
       datasets: [
         {
-          label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          label: "Houses of GOT",
+          data: [
+            houseCount["Stark"],
+            houseCount["Arryn"],
+            houseCount["Baratheon"],
+            houseCount["Tully"],
+            houseCount["Greyjoy"],
+            houseCount["Lannister"],
+            houseCount["Tyrell"],
+            houseCount["Martell"],
+            houseCount["Targaryen"],
+          ],
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
@@ -48,5 +99,16 @@ const renderChart = () => {
     },
   });
 };
+
+async function getCharacters() {
+  console.log("in get characters");
+  const url = "https://thronesapi.com/api/v2/Characters";
+  try {
+    let response = await fetch(url);
+    return await response.json();
+  } catch (erorr) {
+    console.log(error);
+  }
+}
 
 renderChart();
